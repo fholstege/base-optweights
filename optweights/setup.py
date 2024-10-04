3174a84b7e9806ac87caada7a0897f1a90bae4a2
fholstege <31654652+fholstege@users.noreply.github.com>
2024-10-04 15:29:15 +0200

    change structure for testing

--- optweights/setup.py
+++ optweights/setup.py
@@ -1,16 +1,11 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setup(
     name="optweights",
     version="0.1.0",
     author="Floris Holstege",
     author_email="f.g.holstege@uva.nl",
     description="A package for optimizing weights",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     packages=find_packages(exclude=["tests*"]),
     classifiers=[
         "Development Status :: 3 - Alpha",
